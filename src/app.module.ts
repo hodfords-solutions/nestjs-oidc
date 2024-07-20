d29d4041934aa1fc14f60296ad7752b1e9efb1fa
immint023 <nvminh023@gmail.com>
2024-07-20 10:22:28 +0700

    setup src

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+// import { ResponseModule } from '../libs/modules/response.module';
+
+// const nestjsResponseConfig = ResponseModule.forRoot({
+//     excludedKeys: ['secret']
+// });
+
+@Module({
+    imports: [],
+    providers: [],
+    controllers: [AppController]
+})
+export class AppModule {}
