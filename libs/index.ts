33f8880669d1d1127275c5e81e9bede331f50677
immint023 <nvminh023@gmail.com>
2024-07-30 10:25:38 +0700

    feat: export oidc auth service

--- libs/index.ts
+++ libs/index.ts
@@ -2,4 +2,5 @@ export * from './modules/oidc.module';
 export * from './interfaces/account.interface';
 export * from './interfaces/account-service.interface';
 export * from './services/oidc.service';
+export * from './services/oidc-auth.service';
 export * from './constants/injector.constant';
