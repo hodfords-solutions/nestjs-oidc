d77cf6c3a463dc7ed05e568ef0dfdbdf111979a7
Minh Ngo HF <minh.ngo@hodfords.com>
2025-10-07 15:00:06 +0700

    feat: include odic swagger

--- lib/controllers/oidc.controller.ts
+++ lib/controllers/oidc.controller.ts
@@ -1,15 +1,12 @@
 import { All, Controller, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { OidcService } from '../services/oidc.service';
-import { ApiExcludeEndpoint, ApiExcludeController } from '@nestjs/swagger';
 
 @Controller('oidc')
-@ApiExcludeController()
 export class OidcController {
     constructor(private oidcService: OidcService) {}
 
     @All('/{*splat}')
-    @ApiExcludeEndpoint()
     mountedOidc(@Req() req: Request, @Res() res: Response): Promise<void> {
         req.url = req.originalUrl.replace('/oidc', '');
 
